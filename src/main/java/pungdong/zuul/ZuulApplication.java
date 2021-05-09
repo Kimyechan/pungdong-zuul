cadf3b9f7b243604ca41d6651057ccccbdbf46a9
Kimyechan <vlvkcjswo7@naver.com>
2021-05-09 19:35:20 +0900

    Conf: Setting CORS
    allow all

--- src/main/java/pungdong/zuul/ZuulApplication.java
+++ src/main/java/pungdong/zuul/ZuulApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -14,4 +17,17 @@ public class ZuulApplication {
         SpringApplication.run(ZuulApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
 }
