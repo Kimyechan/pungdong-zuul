c13d6c9fefc6da064dd7fb47baf8b370aa40ef82
Kimyechan <vlvkcjswo7@naver.com>
2021-05-10 19:37:39 +0900

    Conf: Add Eureka Client IP
    Add file to aws server

--- src/main/java/pungdong/zuul/ZuulApplication.java
+++ src/main/java/pungdong/zuul/ZuulApplication.java
@@ -2,6 +2,7 @@ package pungdong.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "/home/ubuntu/config/project/zuul/eureka.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(ZuulApplication.class, args);
+        new SpringApplicationBuilder(ZuulApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
     @Bean
