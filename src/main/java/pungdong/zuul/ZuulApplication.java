ac12eecd2115152c51fd9c9105c5726feb202a31
Kimyechan <vlvkcjswo7@naver.com>
2021-05-11 23:21:55 +0900

    Conf: Add Config Location
    Add application.yml

--- src/main/java/pungdong/zuul/ZuulApplication.java
+++ src/main/java/pungdong/zuul/ZuulApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ZuulApplication {
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
             + "/home/ubuntu/config/project/zuul/eureka.yml";
 
     public static void main(String[] args) {
